1b0e5099c50825548d37fb22cf160263542ebfb9
lahh <raihanfazasabiqr@gmail.com>
2025-02-01 14:52:15 +0700

    updating crud

--- logbook_app/models.py
+++ logbook_app/models.py
@@ -1,17 +1,11 @@
 from datetime import datetime as dt
 
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class Log(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    date = models.DateField(default=dt.today().strftime("%d-%m-%Y"))
+    date = models.DateField(default=dt.today())
     details = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-class Player(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- logbook_app/views.py
+++ logbook_app/views.py
@@ -1,21 +1,69 @@
+from django.forms.models import model_to_dict
 from django.http.response import JsonResponse
 from ninja import NinjaAPI
 
 from .models import Log
+from .parser import ORJSONParser
 from .schema import LogbookIn
 
-# Logbook manual crud
-api = NinjaAPI(version="1.0.0")
+api = NinjaAPI(version="1.0.0", parser=ORJSONParser())
 
 
 @api.get("/logbooks")
 async def get_logbooks(request):
-    logbooks = {logbook async for logbook in Log.objects.all()}
+    logbooks = [model_to_dict(logbook) async for logbook in Log.objects.all()]
     res = {"status": 200, "data": logbooks}
     return JsonResponse(res)
 
 
-async def create_logbooks(request, payload: LogbookIn):
-    logbook = await Log.objects.acreate(**payload.dict())
-    res = {"status": 200, "data": logbook}
+@api.get("/logbook")
+async def get_logbook(request, logbook_id):
+    try:
+        logbook = await Log.objects.aget(id=logbook_id)
+        res = {
+            "status": 200,
+            "message": "logbook retrieved.",
+            "data": model_to_dict(logbook),
+        }
+    except:
+        res = {"status": 400, "message": "Invalid Data"}
+    return JsonResponse(res)
+
+
+@api.post("/logbook")
+async def create_logbook(request, payload: LogbookIn):
+    try:
+        logbook = await Log.objects.acreate(**payload.dict())
+        res = {
+            "status": 200,
+            "message": "logbook created.",
+            "data": model_to_dict(logbook),
+        }
+    except:
+        res = {"status": 400, "message": "Invalid Data"}
+    return JsonResponse(res)
+
+
+@api.put("/logbook")
+async def update_logbook(request, logbook_id: int):
+    try:
+        logbook = await Log.objects.aget(id=logbook_id)
+        res = {
+            "status": 200,
+            "message": "logbook updated.",
+            "data": model_to_dict(logbook),
+        }
+    except:
+        res = {"status": 400, "message": "logbook not found."}
+    return JsonResponse(res)
+
+
+@api.delete("/logbook/")
+async def delete_logbook(request, logbook_id: int):
+    try:
+        logbook = await Log.objects.aget(id=logbook_id)
+        await logbook.adelete()
+        res = {"status": 200, "message": "logbook deleted."}
+    except:
+        res = {"status": 400, "message": "logbook not found."}
     return JsonResponse(res)
